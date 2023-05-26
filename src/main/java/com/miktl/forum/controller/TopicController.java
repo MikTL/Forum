643b2ab72c0ccd9a683e00000af9814001b4da87
Miki Tucto Leon <mick01leon@gmail.com>
2023-05-26 16:53:27 -0500

    Implementing a method in TopicService to create a topic, implementing the creation of the topic in the controller

--- src/main/java/com/miktl/forum/controller/TopicController.java
+++ src/main/java/com/miktl/forum/controller/TopicController.java
@@ -1,2 +1,36 @@
-package com.miktl.forum.controller;public class TopicController {
+package com.miktl.forum.controller;
+
+import com.miktl.forum.dto.topic.DataToRegisterTopic;
+import com.miktl.forum.dto.topic.RegisteredDataTopic;
+import com.miktl.forum.service.TopicService;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/topic")
+public class TopicController {
+    private final TopicService topicService;
+    public TopicController(TopicService topicService) {
+        this.topicService = topicService;
+    }
+    @PostMapping
+    @Transactional
+    public ResponseEntity<RegisteredDataTopic>createTopic(
+            @RequestBody
+            @Valid
+            DataToRegisterTopic dataToRegisterTopic,
+            UriComponentsBuilder uriComponentsBuilder
+    ) {
+        RegisteredDataTopic registeredDataTopic = topicService.createTopic(dataToRegisterTopic);
+        URI uri= uriComponentsBuilder.path("/topic/{id}").buildAndExpand(registeredDataTopic.id()).toUri();
+        return ResponseEntity.created(uri).body(registeredDataTopic);
+    }
 }
--- src/main/java/com/miktl/forum/service/TopicService.java
+++ src/main/java/com/miktl/forum/service/TopicService.java
@@ -1,2 +1,45 @@
-package com.miktl.forum.service;public class TopicService {
+package com.miktl.forum.service;
+
+import com.miktl.forum.domain.course.Course;
+import com.miktl.forum.domain.topic.Topic;
+import com.miktl.forum.domain.user.User;
+import com.miktl.forum.dto.topic.DataToRegisterTopic;
+import com.miktl.forum.dto.topic.RegisteredDataTopic;
+import com.miktl.forum.repository.course.CourseRepository;
+import com.miktl.forum.repository.topic.TopicRepository;
+import com.miktl.forum.repository.user.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+    private final TopicRepository topicRepository;
+    private final UserRepository userRepository;
+    private final CourseRepository courseRepository;
+
+    public TopicService(
+            TopicRepository topicRepository,
+            UserRepository userRepository,
+            CourseRepository courseRepository
+    ) {
+        this.topicRepository = topicRepository;
+        this.userRepository = userRepository;
+        this.courseRepository = courseRepository;
+    }
+
+    public RegisteredDataTopic createTopic(DataToRegisterTopic dataToRegisterTopic){
+        Topic topic= topicRepository.save(new Topic(dataToRegisterTopic));
+        User user = userRepository.findById(topic.getId_user()).orElse(null);
+        String authorName= (user!=null)? user.getName() : null;
+        Course course = courseRepository.findById(topic.getId_course()).orElse(null);
+        String courseName=(course!=null)?course.getName():null;
+        return new RegisteredDataTopic(
+                topic.getId(),
+                topic.getTitle(),
+                topic.getMessage(),
+                topic.getCreationDate(),
+                topic.getStatusTopic(),
+                authorName,
+                courseName
+        );
+    }
 }
