854f0b6ce1f624a34c019ddc555469a608bab25a
Miki Tucto Leon <mick01leon@gmail.com>
2023-05-26 16:35:09 -0500

    Creating entity and repository for Courses

--- src/main/java/com/miktl/forum/domain/course/Course.java
+++ src/main/java/com/miktl/forum/domain/course/Course.java
@@ -1,2 +1,15 @@
-package com.miktl.forum.domain.course;public class Course {
+package com.miktl.forum.domain.course;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity(name = "Course")
+@Table(name = "courses")
+@Getter
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String name;
+    String description;
 }
