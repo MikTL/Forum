b35fc3390fe96822bf07be7cd323651e1f101ab1
Miki Tucto Leon <mick01leon@gmail.com>
2023-05-27 22:00:43 -0500

    Adding the id to the DTO and the Query for the topic listing

--- src/main/java/com/miktl/forum/dto/topic/TopicListingData.java
+++ src/main/java/com/miktl/forum/dto/topic/TopicListingData.java
@@ -5,12 +5,11 @@ import com.miktl.forum.domain.topic.StatusTopic;
 import java.time.LocalDateTime;
 
 public record TopicListingData(
+        Long id,
         String title,
         String message,
         LocalDateTime creationDate,
         StatusTopic statusTopic,
         String author,
         String course
-) {
-
-}
+) {}
--- src/main/java/com/miktl/forum/repository/topic/TopicRepository.java
+++ src/main/java/com/miktl/forum/repository/topic/TopicRepository.java
@@ -10,7 +10,7 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface TopicRepository extends JpaRepository<Topic,Long> {
-    @Query("SELECT new com.miktl.forum.dto.topic.TopicListingData(t.title,t.message,t.creationDate,t.statusTopic,u.name,c.name) FROM Topic t JOIN User u ON t.id_user= u.id JOIN Course c ON t.id_course=c.id")
+    @Query("SELECT new com.miktl.forum.dto.topic.TopicListingData(t.id,t.title,t.message,t.creationDate,t.statusTopic,u.name,c.name) FROM Topic t JOIN User u ON t.id_user= u.id JOIN Course c ON t.id_course=c.id")
     Page<TopicListingData> findAllWithAuthorAndCourse(Pageable pageable);
 
 }
