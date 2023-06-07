5067e194b7bd0a5116f38376db20b98aa274f9f7
Miki Tucto Leon <mick01leon@gmail.com>
2023-06-07 14:47:36 -0500

    Creating the service to load user detail during authenticaion

--- src/main/java/com/miktl/forum/repository/user/UserRepository.java
+++ src/main/java/com/miktl/forum/repository/user/UserRepository.java
@@ -2,6 +2,8 @@ package com.miktl.forum.repository.user;
 
 import com.miktl.forum.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User,Long> {
+    UserDetails findByEmail(String email);
 }
