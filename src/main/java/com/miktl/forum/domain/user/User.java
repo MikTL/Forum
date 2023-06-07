24fbfcf7b2177978d5213a3da011500aaaa32721
Miki Tucto Leon <mick01leon@gmail.com>
2023-06-07 14:26:16 -0500

    Implementing UserDetails Interface to stardardize user information for user authentication and Authorization

--- src/main/java/com/miktl/forum/domain/user/User.java
+++ src/main/java/com/miktl/forum/domain/user/User.java
@@ -4,12 +4,18 @@ import com.miktl.forum.dto.user.DataToRegisterUser;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity(name = "User")
 @Table(name = "users")
 @Getter
 @NoArgsConstructor
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -31,5 +37,39 @@ public class User {
     }
     public void logicDelete() {
         this.deleted=true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
